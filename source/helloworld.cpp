3977f16a3f00117d924d1913dd56d4f2fe5767a3
Momena Rahmati <sosan.momeni@gmial.com>
2020-05-08 11:16:17 +0200

    task 1-3

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
+#include<iostream>
+
+int findKvg()
+{
+   
+    int zahl = 20;
+    while(true)
+    {
+        bool is_divisible_by_number = true;
+        for (int i=1; i<20; ++i)
+        {
+            if (0!=(zahl%i))
+            {
+                is_divisible_by_number = false;
+            }
+        }
+        if (is_divisible_by_number)
+        {
+            std::cout<< "The smalles number that is divisible by number 1 to 20 is :" << zahl <<std::endl;
+            break;
+            return zahl;
+        }
+        zahl +=20;
+    }
+}
+
 
 int main()
 {
   std::cout << "Hello, World!\n";
-  return 0;
+ 
+  findKvg();
+  
 }

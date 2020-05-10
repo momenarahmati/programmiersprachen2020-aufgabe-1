6e0420db0d47f7d9a7eb3420103882e89ca35187
Momena Rahmati <sosan.momeni@gmial.com>
2020-05-10 22:15:15 +0200

    gcd_Aufgabe 1.8

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,35 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include<cmath>
+
+int gcd(int a, int b)
+{
+    int h;
+    if (a == 0)
+    {
+        return abs(b); // absolute value of b
+    }
+    if (b == 0)
+    {
+        return abs(a);
+    }
+    do
+    {
+        h = a % b;
+        a = b;
+        b = h;
+    } while (b != 0);
+    return abs(a);
+}
+
+TEST_CASE("describe_gcd", "[gcd]")
+{
+    REQUIRE(gcd(2,4)==2);
+    REQUIRE(gcd(1,0)==1);
+    REQUIRE(gcd(9,6)==3);
+    REQUIRE(gcd(3,7)==1);
+    REQUIRE(gcd(3,-7)==1);
+}
 
 int main(int argc, char* argv[])
 {

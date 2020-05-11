8db15768ee5d6526e35540f5839914ea1a4dc90d
Momena Rahmati <sosan.momeni@gmial.com>
2020-05-11 03:17:35 +0200

    add test to mileToKilometer.cpp

--- source/mile_to_kilometer.cpp
+++ source/mile_to_kilometer.cpp
@@ -1,14 +1,29 @@
 #include <iostream> 
 #include <string>
+# define CATCH_CONFIG_RUNNER
+# include "catch.hpp"
 
-int main() 
+double mileToKilometer(double m)
+{
+    double km = m * 1.609;
+    return km;
+}
+
+TEST_CASE("mileToKilometer_9","[mileToKilometer]")
+{
+    REQUIRE(mileToKilometer(1) == Approx(1.609));
+    REQUIRE(mileToKilometer(0) == Approx(0.0));
+    REQUIRE(mileToKilometer(5) == Approx(8.045));
+}
+
+    int main(int argc, char* argv[])
     {
-        double m = 0.0f;
-        std::cout << "Bitte geben Sie Milen : \n";
-        std::cin >> m;
-        float kilometer = 1.61;
-        double k;
-        k = kilometer * m;
-        std::cout << m << " mile ist " << k << " kilometer \n ";
-        return 0;
-    }
+        return Catch::Session().run(argc, argv);
+    
+    std::cout << "Bitte geben Sie eine Zahl fuer die Umrechnung von Meilen zu Kilometer ein:\n";
+    double m = 0;
+    std::cin>>m;
+    std::cout<<m<< " Meilen sind umgerechnet " << mileToKilometer(m) << " Kilometer.\n";
+
+    return 0;
+}
